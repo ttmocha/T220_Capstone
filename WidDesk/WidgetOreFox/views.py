c4af69659cdf556b4c345d12c232d198e76557b8
Ty Thiemann <162078081+ttmocha@users.noreply.github.com>
2024-08-16 12:34:19 +1000

    Testing HTML with Django

--- WidDesk/WidgetOreFox/views.py
+++ WidDesk/WidgetOreFox/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Hello There!, You're using WidDesk.")
+
+def home(request):
+    return render(request, 'WidgetOreFox/home.html')
+def about(request):
+    return render(request, 'WidgetOreFox/about.html')
+def contact(request):
+    return render(request, 'WidgetOreFox/contact.html')
+#@def index(request):
+  #  return HttpResponse("Hello There!, You're using WidDesk.")
     
 # Create your views here.
